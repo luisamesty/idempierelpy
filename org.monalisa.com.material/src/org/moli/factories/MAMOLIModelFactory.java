a4a00812279a243c2c7e241681420c9442a89b5c
luisamesty <luisamesty@luisamesty@gmail.com>
2026-05-21 18:56:57 +0200

    release-11_LPY-81  Clase MOProduct debe extende de MProduct. Error al grabar LDM

--- org.monalisa.com.material/src/org/moli/factories/MAMOLIModelFactory.java
+++ org.monalisa.com.material/src/org/moli/factories/MAMOLIModelFactory.java
@@ -8,7 +8,7 @@ import org.compiere.model.PO;
 import org.compiere.util.CLogger;
 import org.compiere.util.Env;
 import org.moli.compiere.MO_Warehouse;
-import org.moli.model.MOLI_I_Product;
+import org.moli.compiere.MO_Product;
 
 
 public class MAMOLIModelFactory implements IModelFactory {
@@ -18,8 +18,8 @@ public class MAMOLIModelFactory implements IModelFactory {
 	@Override
 	public Class<?> getClass(String tableName) {
 		// MOLI_I_Product
-		if(tableName.equalsIgnoreCase(MOLI_I_Product.Table_Name))
-			return MOLI_I_Product.class;
+		if(tableName.equalsIgnoreCase(MO_Product.Table_Name))
+			return MO_Product.class;
 		// MO_Warehouse
 		if(tableName.equalsIgnoreCase(MO_Warehouse.Table_Name))
 			return MO_Warehouse.class;
@@ -29,8 +29,8 @@ public class MAMOLIModelFactory implements IModelFactory {
 	@Override
 	public PO getPO(String tableName, int Record_ID, String trxName) {
 		// MOLI_I_Product
-		if(tableName.equalsIgnoreCase(MOLI_I_Product.Table_Name))
-			return new MOLI_I_Product(Env.getCtx(),Record_ID, trxName);
+		if(tableName.equalsIgnoreCase(MO_Product.Table_Name))
+			return new MO_Product(Env.getCtx(),Record_ID, trxName);
 		// MO_Warehouse
 		if(tableName.equalsIgnoreCase(MO_Warehouse.Table_Name))
 			return new MO_Warehouse(Env.getCtx(),Record_ID, trxName);
@@ -40,8 +40,8 @@ public class MAMOLIModelFactory implements IModelFactory {
 	@Override
 	public PO getPO(String tableName, ResultSet rs, String trxName) {
 		// MOLI_I_Product
-		if(tableName.equalsIgnoreCase(MOLI_I_Product.Table_Name))
-			return new MOLI_I_Product(Env.getCtx(),rs, trxName);
+		if(tableName.equalsIgnoreCase(MO_Product.Table_Name))
+			return new MO_Product(Env.getCtx(),rs, trxName);
 		// MO_Warehouse
 		if(tableName.equalsIgnoreCase(MO_Warehouse.Table_Name))
 			return new MO_Warehouse(Env.getCtx(),rs, trxName);
